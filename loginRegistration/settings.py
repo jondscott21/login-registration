d8ad37670dec24328ed0b244b9a97e2f178cd61e
Urian Chang <urianchang@gmail.com>
2017-02-22 13:48:22 -0800

    Modified Settings.py

--- loginRegistration/settings.py
+++ loginRegistration/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'n@vhl3sbllx0ckf-4%x%1*454v4=ivztf$lgj=xxbawo18h_il'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['35.163.41.165']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
